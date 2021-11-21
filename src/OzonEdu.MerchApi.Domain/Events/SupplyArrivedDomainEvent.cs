093def6ef61c3fe217188b8eca9be4eb61ca61dd
Konstantinov Kirill <mr.hypno@yandex.ru>
2021-11-21 07:41:16 +0300

    Extracted methods from SupplyArrivedDomainEventHandler

--- src/OzonEdu.MerchApi.Domain/Events/SupplyArrivedDomainEvent.cs
+++ src/OzonEdu.MerchApi.Domain/Events/SupplyArrivedDomainEvent.cs
@@ -5,14 +5,14 @@ using OzonEdu.MerchApi.Domain.Exceptions;
 
 namespace OzonEdu.MerchApi.Domain.Events
 {
-    public class SupplyArrivedWithStockItemsDomainEvent : INotification
+    public class SupplyArrivedDomainEvent : INotification
     {
         public IReadOnlyDictionary<Sku, MerchItemsQuantity> Items { get; }
         
-        public SupplyArrivedWithStockItemsDomainEvent(IReadOnlyDictionary<Sku, MerchItemsQuantity> items)
+        public SupplyArrivedDomainEvent(IReadOnlyDictionary<Sku, MerchItemsQuantity> items)
         {
             Items = items ?? throw new RequiredEventPropertyIsNullException(nameof(items),
-                $"{nameof(SupplyArrivedWithStockItemsDomainEvent)} can't be created without {nameof(items)}");
+                $"{nameof(SupplyArrivedDomainEvent)} can't be created without {nameof(items)}");
         }
     }
 }
